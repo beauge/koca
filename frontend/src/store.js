78846a6dfe82258bcc38a6e4f44459e34277c995
Nate <nathanbeauge@gmail.com>
2023-02-18 15:36:08 -0600

    Setup error handlers, redux store, create reducer, constants, and actions.

---
+++ frontend/src/store.js
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import { productListReducer } from './reducers/productReducers';
+
+const reducers = {
+  productList: productListReducer,
+};
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = configureStore({
+  reducer: reducers,
+  preloadedState: initialState,
+  middleware: middleware,
+});
+
+export default store;
